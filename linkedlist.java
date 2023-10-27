1574bf886cecb5f19bd35748278ff85eab0575d2
Althaf Nazeer <119403840+Althaf-Nazeer@users.noreply.github.com>
2023-10-27 19:53:13 +0530

    Create linkedlist.java

---
+++ linkedlist.java
@@ -0,0 +1,28 @@
+public class LinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        printList(head);
+    }
+}
